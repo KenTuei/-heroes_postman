142e24b75438d14f673f8887159ff407ab64fec9
ken <kentuei05@gmail.com>
2025-06-14 15:00:04 +0300

    intergrate Flask-migrate and apply first migration

--- server/app.py
+++ server/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    ...
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Import models so they're registered with SQLAlchemy
+    from . import models
+
     return app
-
-
-db.init_app(app)
